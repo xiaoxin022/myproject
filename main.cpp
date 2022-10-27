3cae86604d2ccf9cd7790a91c1825a99634de9fc
xiaoxin022 <xiaoxin871213@163.com>
2022-10-28 01:20:10 +0800

    update

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "process.h"
 
 Client client;
+Paint paint;
 Memory memory;
 
 HANDLE hProcess;
@@ -22,62 +23,112 @@ struct Mat4x4 {
 
 class Player {
 public:
-    //位置x,y,z
-    Vec3 location;
-    //坐标矩阵
-    Mat4x4 mat;
+    //世界坐标 x,y,z
+    Vec3 world;
+    //屏幕坐标 x,y,z
+    Vec3 screen;
     float hp;
     int num;
+    //阵营
+    int flag;
 public:
     Player(int num) {
         this->num = num;
+        getFlag(hProcess);
     };
 
     Player() {};
 
+    static float getDistance(Vec3 m, Vec3 p){
+        return sqrt(powf(m.x - p.x, 2) + powf(m.y - p.y, 2) + powf(m.z - p.z, 2));
+    };
+
     void getHp(HANDLE hProcess) {
         DWORD first = memory.readDWORD(hProcess, client.base_cstrike_exe + 0x11069BC);
-        DWORD second = memory.readDWORD(hProcess, first + 0x7c);
+        DWORD second = memory.readDWORD(hProcess, first + num * 0x324 + + 0x7c);
         DWORD third = memory.readDWORD(hProcess, second + 0x4);
         memory.readFloat(hProcess, third + 0x160, &hp);
-        cout << "blood=" << hp << endl;
     };
 
     void getLocation(HANDLE hProcess) {
         DWORD first = memory.readDWORD(hProcess, client.base_cstrike_exe + 0x11069BC);
-        memory.readFloat(hProcess, first + num * 0x324 + 0x88, &location.x);
-        memory.readFloat(hProcess, first + num * 0x324 + 0x8c, &location.y);
-        cout << "x=" << location.x << endl;
-        cout << "y=" << location.y << endl;
+        memory.readFloat(hProcess, first + num * 0x324 + 0x88, &world.x);
+        memory.readFloat(hProcess, first + num * 0x324 + 0x8c, &world.y);
+        memory.readFloat(hProcess, first + num * 0x324 + 0x90, &world.z);
+    }
+
+    void getFlag(HANDLE hProcess) {
+        DWORD first = memory.readDWORD(hProcess, client.base_cstrike_exe + 0x11069BC);
+        DWORD second =memory.readDWORD(hProcess, first + num * 0x324 + 0x7c);
+        memory.readFloat(hProcess, second + 0x1c8, &flag);
     }
 };
 
-int player_nums = 1;
+int player_nums = 3;
 Player players[20];
+Mat4x4 mat;
 
 DWORD getPlayerNums(HANDLE hProcess) {
-    player_nums = memory.readFloat(hProcess, client.base_mp_dll + 0x114960, &player_nums);
-    return player_nums;
+//    player_nums =  memory.readFloat(hProcess, client.base_mp_dll + 0x114960, &player_nums);
+return player_nums;
+}
+
+void updateMat() {
+    memory.readFloat(hProcess, client.base_cstrike_exe + 0x1820100, &mat.a, sizeof(mat.a));
 }
 
 void init() {
-    client.getProcess("cstrike.exe");
-    client.base_cstrike_exe = client.getModuleArr("cstrike.exe");
-    client.base_mp_dll = client.getModuleArr("mp.dll");
     hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, client.dProcessId);
 }
 
 int main() {
     init();
     for (int i = 0; i < 20; ++i) {
-        Player player(i + 1);
+        Player player(i);
         players[i] = player;
     }
+
     while (true) {
-        for (int i = 0; i < player_nums; ++i) {
+        for (int i = 1; i <= player_nums; ++i) {
+            if (players[i].flag == players[0].flag){
+                continue;
+            }
             players[i].getHp(hProcess);
-            cout << "hp=" << players[i].hp << endl;
+            if (players[i].hp <= 0){
+                continue;
+            }
+            updateMat();
+
+            //cout << "hp=" << players[i].hp << endl;
             players[i].getLocation(hProcess);
+            players[i].screen.z =
+                    mat.a[2] * players[i].world.x + mat.a[6] * players[i].world.y + mat.a[10] * players[i].world.z +
+                    mat.a[14];
+            if (players[i].screen.z < 0.001f) {
+                continue;
+            }
+            players[i].screen.x = paint.m_HalfGameWidth +
+                                  (mat.a[0] * players[i].world.x + mat.a[4] * players[i].world.y +
+                                   mat.a[8] * players[i].world.z + mat.a[12]) / players[i].screen.z *
+                                  paint.m_HalfGameWidth;
+            players[i].screen.y = paint.m_HalfGameHigh -
+                                  (mat.a[1] * players[i].world.x + mat.a[5] * players[i].world.y +
+                                   mat.a[9] * players[i].world.z + mat.a[13]) / players[i].screen.z *
+                                  paint.m_HalfGameHigh;
+            RECT rect;
+            rect.left = players[i].screen.x;
+            rect.top = players[i].screen.y;
+            rect.right = players[i].screen.x + 100;
+            rect.bottom = players[i].screen.y + 40;
+            LPRECT lprc = (struct tagRECT *) malloc(sizeof(struct tagRECT));
+            lprc->left = rect.left;
+            lprc->right = rect.left + 100;
+            lprc->top = rect.top;
+            lprc->bottom = rect.top + 40;
+            float distance = Player::getDistance(players[0].world, players[i].world);
+            DrawText(paint.m_hdc, distance, -1, lprc, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+            Rectangle(paint.m_hdc, rect.left, rect.top, rect.right, rect.bottom);
+            cout << "x=" << players[i].screen.x << ",y=" << players[i].screen.y << endl;
         }
         player_nums = getPlayerNums(hProcess);
     }
--- process.h
+++ process.h
@@ -11,29 +11,69 @@ using namespace std;
 
 class Memory {
 public:
-    inline BOOL readDWORD(HANDLE hProcess, DWORD lpBaseAddress){
+    inline BOOL readDWORD(HANDLE hProcess, DWORD lpBaseAddress) {
         DWORD lpBuffer;
-        ReadProcessMemory(hProcess, (LPCVOID)lpBaseAddress, &lpBuffer, 4, NULL);
-        return  lpBuffer;
+        ReadProcessMemory(hProcess, (LPCVOID) lpBaseAddress, &lpBuffer, 4, NULL);
+        return lpBuffer;
     };
-    template <typename T>
-    inline BOOL readFloat(HANDLE hProcess, DWORD lpBaseAddress,T lpBuffer){
-        return ReadProcessMemory(hProcess, (LPCVOID)lpBaseAddress, lpBuffer, 4, NULL);
+
+    template<typename T>
+    inline BOOL readFloat(HANDLE hProcess, DWORD lpBaseAddress, T lpBuffer) {
+        return ReadProcessMemory(hProcess, (LPCVOID) lpBaseAddress, lpBuffer, 4, NULL);
+    };
+
+    template<typename T>
+    inline BOOL readFloat(HANDLE hProcess, DWORD lpBaseAddress, T lpBuffer, SIZE_T size) {
+        return ReadProcessMemory(hProcess, (LPCVOID) lpBaseAddress, lpBuffer, size, NULL);
     };
 
 
-    inline BOOL write(HANDLE hProcess, LPVOID lpBaseAddress, DWORD lpBuffer){
-        return WriteProcessMemory(hProcess, lpBaseAddress, (LPCVOID)lpBuffer, sizeof(lpBuffer), NULL);
+    inline BOOL write(HANDLE hProcess, LPVOID lpBaseAddress, DWORD lpBuffer) {
+        return WriteProcessMemory(hProcess, lpBaseAddress, (LPCVOID) lpBuffer, sizeof(lpBuffer), NULL);
     };
+};
+
+class Paint {
+public:
+    HDC m_hdc;
+    HWND hWnd;
+    int m_HalfGameWidth;
+    int m_HalfGameHigh;
+    RECT m_GameRect;
+public:
+    Paint() {
+        hWnd = FindWindow(NULL, "Counter-Strike");
+        // 获取屏幕客户端界面
+        GetClientRect(hWnd, &m_GameRect);
+        m_HalfGameWidth = (m_GameRect.right - m_GameRect.left) * 0.5;
+        m_HalfGameHigh = (m_GameRect.bottom - m_GameRect.top) * 0.5;
+
+        // 设置画笔颜色，画出来的线条就是这个颜色
+        HPEN hPen = CreatePen(PS_SOLID, 2, RGB(127, 255, 0));
+        HBRUSH hBrush = (HBRUSH) GetStockObject(NULL_BRUSH);
+        m_hdc = GetDC(hWnd);
+        // 设置字体颜色
+        SetTextColor(m_hdc, RGB(127, 255, 0));
+        // 去除背景底色
+        SetBkMode(m_hdc, TRANSPARENT);
+        SelectObject(m_hdc, hPen);
+        SelectObject(m_hdc, hBrush);
+    }
 };
 
 class Client {
 public:
     DWORD dProcessId;
-    HWND hWnd;
     DWORD base_cstrike_exe;
     DWORD base_mp_dll;
+
 public:
+    Client() {
+        getProcess("cstrike.exe");
+        base_cstrike_exe = getModuleArr("cstrike.exe");
+        base_mp_dll = getModuleArr("mp.dll");
+    }
+
     void getProcess(char processName[]) {
         HANDLE handle = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
         PROCESSENTRY32 processentry32 = {sizeof(processentry32)};
@@ -46,18 +86,20 @@ public:
             }
         } while (Process32Next(handle, &processentry32));
     }
-    DWORD getModuleArr(char moduleName[]){
+
+    DWORD getModuleArr(char moduleName[]) {
         HMODULE hModule;
         HANDLE handle = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, dProcessId);
         MODULEENTRY32 moduleentry32 = {sizeof(moduleentry32)};
-        Module32First(handle,&moduleentry32);
+        Module32First(handle, &moduleentry32);
         do {
+            cout << moduleentry32.szModule << endl;
             if (strcmp(moduleName, moduleentry32.szModule) == 0) {
                 cout << "ModuleName=" << moduleentry32.szModule << endl;
                 cout << "ModuleArr=" << moduleentry32.hModule << endl;
                 hModule = moduleentry32.hModule;
             }
         } while (Module32Next(handle, &moduleentry32));
-        return (DWORD)hModule;
+        return (DWORD) hModule;
     }
 };
